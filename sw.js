13f943efa853343477b68433bf378226920a6dac
Fuxuan-CN <3072252442@qq.com>
2026-02-07 23:02:07 +0800

    Site updated: 2026-02-07 23:02:03

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1770476022478";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1770476519823";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
